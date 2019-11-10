0a3f6da968c3e446881694b7668d7cddaf0f4b59
Davesmall28 <dave@dsmall.co.uk>
2019-11-10 21:14:50 +0000

    Build script

---
+++ Springboard365.CrmSvcUtil.SingleEntity/NuGet/NuGetPush.bat
@@ -0,0 +1,7 @@
+SET packageVersion=1.0.0-alpha001
+
+NuGet.exe pack ../Springboard365.CrmSvcUtil.SingleEntity.csproj -Build -symbols -Version %packageVersion%
+
+NuGet.exe push Springboard365.CrmSvcUtil.Extensions.%packageVersion%.nupkg -Source https://api.nuget.org/v3/index.json
+
+pause
